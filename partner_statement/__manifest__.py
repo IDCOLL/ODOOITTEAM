30bb385dee8a19b589a86cb846945ecf901f0555
IDCOLL <ian007collins@hotmail.com>
2023-02-06 22:38:59 +0200

    Add files via upload

---
+++ partner_statement/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright 2018 ForgeFlow, S.L. (http://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Statement",
+    "version": "15.0.0.0.0",
+    "category": "Accounting & Finance",
+    "summary": "OCA Financial Reports",
+    "author": "AKTIV SOFTWARE",
+    "website": "http://www.aktivsoftware.com",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "external_dependencies": {"python": ["dateutil"]},
+    "data": [
+        "security/statement_security.xml",
+        "security/ir.model.access.csv",
+        "views/activity_statement.xml",
+        "views/outstanding_statement.xml",
+        "views/aging_buckets.xml",
+        "views/res_config_settings.xml",
+        "wizard/statement_wizard.xml",
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            '/partner_statement/static/src/scss/layout_statement.scss',
+        ]
+    },
+    "installable": True,
+    "application": False,
+}
