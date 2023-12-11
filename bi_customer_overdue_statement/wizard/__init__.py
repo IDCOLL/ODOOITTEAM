d88e56270d0063988becf407c82834623a50fc05
IDCOLL <ian007collins@hotmail.com>
2023-12-11 12:48:04 +0200

    Delete bi_customer_overdue_statement directory

--- bi_customer_overdue_statement/wizard/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-
-from . import res_config_settings
-# from . import account_report_common
-
-from . import outstanding_report_wizard
-from . import send_overdue_statement
