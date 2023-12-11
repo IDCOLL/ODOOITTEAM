d72616e047f12fe1661ea94524ab572bee924574
IDCOLL <ian007collins@hotmail.com>
2023-12-11 12:48:15 +0200

    Delete partner_statement directory

--- partner_statement/models/account_move_line.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-
-class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
-
-    account_internal_type = fields.Selection(
-        store=True, related='account_id.account_type', string="Internal Type", readonly=True)
