daa981af4e249fa4c596dd318fd0f31aadab5bad
IDCOLL <ian007collins@hotmail.com>
2025-12-10 15:06:54 +0200

    Fix settings v2

--- account_reports_fix/models/res_config_settings.py
+++ account_reports_fix/models/res_config_settings.py
@@ -1,8 +1,10 @@
 from odoo import fields, models
 
+
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
+    # Aging report fields
     default_aging_type = fields.Selection(
         selection_add=[],
         default_model='account.aged.partner.balance.report.handler',
@@ -17,5 +19,17 @@ class ResConfigSettings(models.TransientModel):
     )
     
     default_aging_bucket_duration = fields.Integer(
+        default_model='account.aged.partner.balance.report.handler',
+    )
+    
+    default_filter_partners_non_due = fields.Boolean(
+        default_model='account.aged.partner.balance.report.handler',
+    )
+    
+    default_filter_partners_non_zero = fields.Boolean(
+        default_model='account.aged.partner.balance.report.handler',
+    )
+    
+    default_filter_partners_only_with_moves = fields.Boolean(
         default_model='account.aged.partner.balance.report.handler',
     )
