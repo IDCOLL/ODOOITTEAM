2ccc720741a5a141e992f136c4f213e28a387e71
IDCOLL <ian007collins@hotmail.com>
2025-12-10 15:10:40 +0200

    New Settings fix approach

--- account_reports_fix/models/res_config_settings.py
+++ account_reports_fix/models/res_config_settings.py
@@ -1,35 +1,38 @@
-from odoo import fields, models
+from odoo import models
+from odoo.addons.base.models.res_config import ResConfigModuleInstallationMixin
 
 
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+original_get_classified_fields = ResConfigModuleInstallationMixin._get_classified_fields
 
-    # Aging report fields
-    default_aging_type = fields.Selection(
-        selection_add=[],
-        default_model='account.aged.partner.balance.report.handler',
-    )
+
+def _get_classified_fields_fixed(self, fnames):
+    """Patch to handle fields with missing default_model attribute."""
+    result = {
+        'config': [],
+        'default': [],
+        'model': [],
+        'other': [],
+    }
     
-    default_show_aging_buckets = fields.Boolean(
-        default_model='account.aged.partner.balance.report.handler',
-    )
+    for fname in fnames:
+        field = self._fields.get(fname)
+        if not field:
+            continue
+            
+        if fname.startswith('default_'):
+            if hasattr(field, 'default_model') and field.default_model:
+                result['default'].append(fname)
+            else:
+                # Skip fields without default_model instead of raising
+                result['other'].append(fname)
+        elif fname.startswith('config_'):
+            result['config'].append(fname)
+        elif fname.startswith('module_'):
+            result['model'].append(fname)
+        else:
+            result['other'].append(fname)
     
-    default_aging_bucket_count = fields.Integer(
-        default_model='account.aged.partner.balance.report.handler',
-    )
-    
-    default_aging_bucket_duration = fields.Integer(
-        default_model='account.aged.partner.balance.report.handler',
-    )
-    
-    default_filter_partners_non_due = fields.Boolean(
-        default_model='account.aged.partner.balance.report.handler',
-    )
-    
-    default_filter_partners_non_zero = fields.Boolean(
-        default_model='account.aged.partner.balance.report.handler',
-    )
-    
-    default_filter_partners_only_with_moves = fields.Boolean(
-        default_model='account.aged.partner.balance.report.handler',
-    )
+    return result
+
+
+ResConfigModuleInstallationMixin._get_classified_fields = _get_classified_fields_fixed
