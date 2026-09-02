f8c2c319b71bb08cce92a3684584f93ba4cd2d3a
IDCOLL <ian007collins@hotmail.com>
2026-09-02 15:00:11 +0200

    Remove Folders

--- account_reports_fix/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Account Reports Aging Fix',
-    'version': '17.0.1.0.0',
-    'category': 'Accounting',
-    'summary': 'Fixes missing default_model on default_aging_type field',
-    'depends': ['account_reports'],
-    'auto_install': True,
-    'license': 'LGPL-3',
-}
