ae13ccd1c431e360055be3cade9a8950506cd815
IDCOLL <ian007collins@hotmail.com>
2026-09-02 14:29:38 +0200

    Remove all addons

--- partner_statement/models/account_move_line.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
-
-    # Note: In Odoo 17, account_type is already available directly on account_id.account_type
-    # No additional fields needed
